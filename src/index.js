f8183644d35f464c4333fdc4c9d40c8a26e52598
shimjith <shimjithsks@gmail.com>
2025-07-06 14:15:49 +0530

    added bootstrap import

--- src/index.js
+++ src/index.js
@@ -5,6 +5,8 @@ import 'bootstrap/dist/css/bootstrap.min.css';
 import 'aos/dist/aos.css';
 import './App.css';
 import 'bootstrap-icons/font/bootstrap-icons.css';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import 'bootstrap/dist/js/bootstrap.bundle.min.js';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(<App />);
